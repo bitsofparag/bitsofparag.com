95ea58698593fb24b49f74c3e43a9b1630168a81
Parag M <admin@bitsofparag.com>
2019-12-25 01:07:34 +0100

    Add purgecss to remove unused styles

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
     require('postcss-import')({}),
     require('postcss-url')({ url: 'copy', useHash: true }),
     require('tailwindcss'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./**/*.html']
+    }),
     require('cssnano')({
       preset: 'default',
     }),
