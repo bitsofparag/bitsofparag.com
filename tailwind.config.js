8390ba5d824ee144dffce80a5d4771466ce564bb
Parag M <admin@bitsofparag.com>
2020-05-31 21:23:53 +0200

    Update src styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,22 @@ const { colors } = require('tailwindcss/defaultTheme');
 module.exports = {
   theme: {
     fontFamily: {
-      heading: ['Inter', 'sans-serif'],
-      text: ['Inter', 'sans-serif'],
-      blog: ['Inter', 'sans-serif']
+      display: ['Inter var', 'sans-serif'],
+      heading: ['Inter var', 'sans-serif'],
+      text: ['Inter var', 'sans-serif'],
+      blog: ['Inter var', 'sans-serif']
     },
     fontSize: {
-      'h1': '4.6rem',
-      'h2': '3.6rem',
-      'h3': '2.8rem',
-      'h4': '2.2rem',
+      'display': '4rem',
+      'h1': '2.6rem',
+      'h2': '2rem',
+      'h3': '1.3rem',
+      'h4': '1.1rem',
       'button-text': '2.2rem',
-      'body': '1rem',
-      'body2': '1.125rem',
+      'body': '0.94rem',
+      'body2': '1.05rem',
       'body-large': '1.325rem',
-      'info': '0.85rem',
+      'info': '0.82rem',
       'small': '0.7rem',
     },
     container: {
@@ -24,12 +26,13 @@ module.exports = {
       padding: '1.5rem',
     },
     lineHeight: {
-      'h1': '2.6rem',
-      'h2': '2.2rem',
+      'display': '4.6rem',
+      'h1': '3.2rem',
+      'h2': '2.6rem',
       'h3': '1.8rem',
       'h4': '1.625rem',
       'button-text': '26px',
-      'body': '1.6rem',
+      'body': '1.5rem',
       'body2': '1.85rem',
       'body-large': '2rem',
       'info': '1.125rem',
