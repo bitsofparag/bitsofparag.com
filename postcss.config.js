ebcb1221147bd522311088a44c21f2c96db8e85b
Parag M <admin@bitsofparag.com>
2022-01-27 13:32:53 +0100

    Update package.json - postpone purging css until bug fixed
    https://github.com/FullHuman/purgecss/issues/836

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   plugins: [
-    require('postcss-import')({}),
+    require('postcss-import'),
     //require('postcss-url')({ url: 'copy', useHash: true }),
     require('tailwindcss'),
-    require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./**/*.html'],
-    }),
     require('cssnano')({
       preset: 'default',
     }),
