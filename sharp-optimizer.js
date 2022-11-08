64e1c9f2bd9a5eb0b0c5d484d85657959cfaeb34
Parag M <admin@bitsofparag.com>
2022-11-08 17:18:12 +0100

    Update sharp optimizer script

--- sharp-optimizer.js
+++ sharp-optimizer.js
@@ -1,28 +1,20 @@
 const path = require('path');
 const fs = require('fs');
-const mainImagesDir = path.join(
-  __dirname,
-  'dist',
-  'static',
-  'images',
-  'main'
-);
+const imagesDir = path.join(__dirname, 'dist', 'static', 'images');
+const mainImagesDir = path.join(imagesDir, 'main');
+const notesImagesDir = path.join(imagesDir, 'notes');
 const sharp = require('sharp');
 
-function writeImage(err, buffer, info) {
 
-  if (err) {
-    console.log('Error in optimization', err);
-    return;
-  }
+function writeImage(buffer, info) {
+  const imageName = path.basename(info.imagePath);
+  const imageDir = path.dirname(info.imagePath);
+  const outName = imageName.replace(/\.\S+/, `.${info.format}`);
+  const outPath = `${imageDir}/${outName}`;
 
-  const outName = info.imageName.replace(/\.\S+/, `.${info.format}`);
-  const outDir = info.imageDir;
-  const outPath = `${outDir}/${outName}`;
-
-  fs.writeFile(outPath, buffer, function(e) {
-    if (e) {
-      console.log('Error in writing ' + outPath + ' to disk:', e);
+  fs.writeFile(outPath, buffer, function(err) {
+    if (err) {
+      console.log('Error in writing ' + outPath + ' to disk:', err);
       return;
     }
 
@@ -30,20 +22,34 @@ function writeImage(err, buffer, info) {
   });
 }
 
-fs.readdir(mainImagesDir, (err, images) => {
-  if (err) {
-    console.log('Error reading main images.');
-  } else {
-    images.forEach(function(image) {
-      const imagePath = path.join(mainImagesDir, image);
-      const imageName = path.basename(imagePath);
-      const imageDir = path.dirname(imagePath);
 
-      sharp(imagePath).webp().toBuffer((err, buffer, info) => {
-        writeImage(err, buffer, { imageDir, imageName, ...info });
-      });
+function getAllFilePaths(dirPath, arrayOfFiles = []) {
+  files = fs.readdirSync(dirPath)
+
+  files.forEach(file => {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFilePaths(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(dirPath, "/", file))
+    }
+  })
+
+  return arrayOfFiles
+}
+
+
+[mainImagesDir, notesImagesDir].forEach(folder => {
+  const imagePaths = getAllFilePaths(folder);
+
+  imagePaths.forEach(imagePath => {
+    sharp(imagePath).webp().toBuffer((err, buffer, info) => {
+      if (err) {
+        console.log('Error in optimization', err);
+        return;
+      }
+      writeImage(buffer, { imagePath, ...info });
     });
-  }
+  });
 });
 
 const promises = [];
