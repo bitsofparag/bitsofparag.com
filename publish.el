b1dd81911c84aebc1bd85d8ec0d4cbdc259b182e
Parag M <admin@bitsofparag.com>
2026-09-04 01:55:42 +0200

    Update publish scripts for refactored site

--- publish.el
+++ publish.el
@@ -26,10 +26,10 @@
 ;;   )
 
 (defconst bip-url-home "https://bitsofparag.com/"
-  "The home page url of the website.")
+  "The home page URL of the website.")
 (defconst bip-title "Parag's Personal Website"
   "Title of the website.")
-(defconst bip-desc "Insights from Parag Majumdar, a software developer and engineering manager with 16+ years of experience in startups and SMEs. Explore his projects, writings and connect via email and social media."
+(defconst bip-desc "Writings and microblog posts by Parag Majumdar."
   "Description of the website.")
 (defconst bip-keywords "parag, parag m, blog, opinion, thoughts, technology, experiments"
   "Website keywords for SEO.")
@@ -71,7 +71,7 @@
 ;; Derived from https://nicolasknoebber.com/posts/blogging-with-emacs-and-org.html
 (defun bip-format-sitemap-entry (entry _style project)
   "Format ENTRY in PROJECT.
-Excludes rss.org file in page-src."
+Formats the entry title and publication date."
   (if (equal "rss.org" entry) ""
     (format "[[file:%s][%s]] =%s="
 	    entry
@@ -84,43 +84,35 @@ Excludes rss.org file in page-src."
 ;;   (replace-regexp-in-string "&[lg]t;" "" timestamp)
 ;; )
 
-;; --------------------------------
-;; Custom rss feed generator code.
-(defun bip-format-rss-feed-entry (entry style project)
-  "Format ENTRY for the RSS feed in orgmode.
-ENTRY is a file name.
-STYLE is either 'list' or 'tree'.
-PROJECT is the current project."
-  (let* (
-         (title (org-publish-find-title entry project))
-         (link (concat (file-name-sans-extension entry) ".html"))
-         (pubdate (format-time-string (car org-time-stamp-formats)
-          (org-publish-find-date entry project))))
-    ;;(message "%s %s" pubdate entry)
-    (format "* %s
-:PROPERTIES:
-:RSS_PERMALINK: %s
-:PUBDATE: %s
-:END:\n"
-            title
-            link
-            ;;(concat bip-url-home link)
-            pubdate)))
+;; ---------------------------------------
+;; Custom RSS feed generator code.
+(defun bip-format-rss-feed-entry (entry _style project)
+  "Format public content ENTRY for the RSS feed.
+Only writings and microblog entries belong in the feed."
+  (when (or (string-prefix-p "blog/" entry)
+            (string-prefix-p "microblog/" entry))
+    (let* ((title (org-publish-find-title entry project))
+           (link (concat (file-name-sans-extension entry) ".html"))
+           (pubdate (format-time-string
+                     (car org-time-stamp-formats)
+                     (org-publish-find-date entry project))))
+      (format "* %s\n:PROPERTIES:\n:RSS_PERMALINK: %s\n:PUBDATE: %s\n:END:\n"
+              title
+              link
+              pubdate))))
 
 (defun bip-generate-rss-feed (title list)
-  "Generate RSS feed in orgmode format, as a string.
-TITLE is the title of the RSS feed.
-LIST is an internal representation for the files to include,
-as returned by `org-list-to-subtree'."
+  "Generate an Org source document for the RSS feed.
+TITLE is the feed title. LIST contains sitemap entries."
   (concat "#+TITLE: " title "\n\n"
           (org-list-to-subtree list 1 '(:icount "" :istart ""))))
 
 (defun bip-org-rss-publish-to-rss (plist filename pub-dir)
-  "Publish RSS with PLIST, only when FILENAME is 'rss.org'.
-PUB-DIR is when the output will be placed."
-  (if (equal "rss.org" (file-name-nondirectory filename))
-      (org-rss-publish-to-rss plist filename pub-dir)))
-
+  "Publish the generated RSS sitemap as XML.
+PLIST contains project settings. FILENAME is the sitemap source.
+PUB-DIR is the output directory."
+  (when (equal "rss.org" (file-name-nondirectory filename))
+    (org-rss-publish-to-rss plist filename pub-dir)))
 
 ;; ---------------------------------------
 ;; Publish list
@@ -132,7 +124,7 @@ PUB-DIR is when the output will be placed."
              :recursive t
              :publishing-function 'org-html-publish-to-html
              :publishing-directory (concat bip-root "dist")
-             :exclude (regexp-opt '(".*/node_modules/.*" "README" "blog" "drafts" "yml" "page-src" ".setup"))
+             :exclude (regexp-opt '(".*/node_modules/.*" "README" "blog" "microblog" "notes" "now" "rss" "drafts" "yml" "page-src" ".setup"))
              :auto-sitemap nil
              :html-doctype "html5"
              :language "en"
@@ -154,7 +146,7 @@ PUB-DIR is when the output will be placed."
              :html5-fancy: t
              :tex t
              :sitemap-sort-files 'anti-chronologically)
-       (list "bitsofparag-blog"
+       (list "bitsofparag-writings"
              :base-directory (concat bip-root "page-src/blog")
              :base-extension "org"
              :recursive t
@@ -182,7 +174,7 @@ PUB-DIR is when the output will be placed."
              :html5-fancy: t
              :auto-sitemap t
              :sitemap-filename "index.org"
-             :sitemap-title "Latest posts"
+             :sitemap-title "Writings"
              :sitemap-format-entry 'bip-format-sitemap-entry
              :sitemap-style 'list
              :sitemap-sort-files 'anti-chronologically
@@ -196,7 +188,7 @@ PUB-DIR is when the output will be placed."
              :exclude "index.org~"
              :html-doctype "html5"
              :language "en"
-             :title "From the microblog"
+             :title "Microblog"
              :description "Parag's microblog, a Tumblr-like feed where he shares small updates - thoughts, learnings, photos, sketches, interesting links, favorite quotes and other fragments of information."
              :keywords bip-keywords
              :with-date t
@@ -215,45 +207,15 @@ PUB-DIR is when the output will be placed."
              :html5-fancy: t
              :auto-sitemap nil
              )
-       (list "bitsofparag-notes"
-             :base-directory (concat bip-root "page-src/notes")
-             :base-extension "org"
-             :publishing-directory (concat bip-root "dist/notes")
-             :recursive t
-             :publishing-function 'org-html-publish-to-html  ;; Output directory
-             :exclude "index.org~"
-             :html-doctype "html5"
-             :language "en"
-             :description bip-desc
-             :keywords bip-keywords
-             :with-date t
-             :html-head-include-default-style nil      ;; Do not include predefined header scripts.
-             :html-head-include-scripts nil
-             :html-head *weblog-html-common-head*
-             :html-head-extra *weblog-html-extra-head*
-             :html-preamble *weblog-html-preamble*
-             :html-container "section"
-             :html-container-class "blog"
-             :html-postamble *weblog-html-postamble*
-             :html-link-use-abs-url nil
-             :htmlized-source t
-             :html-scripts: nil
-             :html-style: t
-             :html5-fancy: t
-             :auto-sitemap t
-             :sitemap-filename "index.org"
-             :sitemap-title "Notes"
-             :sitemap-format-entry 'bip-format-sitemap-entry
-             :sitemap-style 'list
-             :sitemap-sort-files 'anti-chronologically
-             )
        (list "bitsofparag-rss"
              :base-directory (concat bip-root "page-src")
              :base-extension "org"
              :publishing-directory (concat bip-root "dist")
              :publishing-function 'bip-org-rss-publish-to-rss
              :recursive t
-             :exclude (regexp-opt '("404.org" "colophon.org" "rss.org" "index.org" "drafts" "blog/index.org" "notes/index.org" "microblog/index.org"))
+             :exclude (regexp-opt '("404.org" "colophon.org" "gpg.org" "index.org"
+                                    "now.org" "notes" "privacy.org" "rss.org"
+                                    "drafts" "blog/index.org" "microblog/index.org"))
              :table-of-contents nil
              :rss-image-url (concat bip-url-home "static/images/og-logo.png")
              :rss-extension "xml"
@@ -270,8 +232,7 @@ PUB-DIR is when the output will be placed."
              :sitemap-function 'bip-generate-rss-feed
              :sitemap-format-entry 'bip-format-rss-feed-entry
              :author user-full-name
-             :email ""
-             )
+             :email "")
        (list "bitsofparag-static"
              :base-directory (concat bip-root "site-assets")
              :base-extension site-attachments
@@ -285,14 +246,13 @@ PUB-DIR is when the output will be placed."
              :publishing-function 'org-publish-attachment
              :recursive t)
        (list "website" :components '("bitsofparag"
-                                     "bitsofparag-blog"
+                                     "bitsofparag-writings"
                                      "bitsofparag-microblog"
-                                     "bitsofparag-notes"
                                      "bitsofparag-static"
                                      "bitsofparag-misc"
                                      ))
-       (list "rss" :components '("bitsofparag-rss"))))
-       ;; TODO rss list was added separately because CF fails to build rss file.
+       (list "rss" :components '("bitsofparag-rss"))
+       ))
 
 (provide 'publish)
 ;;; publish.el ends here
